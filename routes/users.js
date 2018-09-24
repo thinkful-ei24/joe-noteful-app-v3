8f2dab2f34e76fda9eb7d83e1d5097b727d527db
Joe-Pena <joepena@protonmail.com>
2018-09-24 14:31:10 -0600

    Add whitespace check and length check for username/pass

--- routes/users.js
+++ routes/users.js
@@ -17,14 +17,30 @@ router.post('/', (req, res, next) => {
     return next(err);
   }
 
+  
+  if(!username || !password) {
+    const err = new Error('Invalid username/password');
+    err.status = 400;
+    next(err);
+  }
+  
   if (typeof username !== 'string' || typeof password !== 'string') {
     const err = new Error('Username or password are not valid characters');
     err.status = 400;
     return next(err);
   }
 
-  if(!username || !password) {
-    const err = new Error('Invalid username/password');
+  if (username.indexOf(' ') === 0 || 
+      username.indexOf(' ') === username.length - 1 ||
+      password.indexOf(' ') === 0 || 
+      password.indexOf(' ') === password.length - 1) {
+    const err = new Error('No whitespace allowed before or after username/password');
+    err.status = 400;
+    next(err);
+  }
+
+  if (username.length <= 1 || password.length <= 8 || password.length >= 72) {
+    const err = new Error('username must be at least 1 character long, password should be at least 8 or 72 max');
     err.status = 400;
     next(err);
   }
