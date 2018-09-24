83c683858bfb36ccbf86ebb373d10c76a6b6a854
Joe-Pena <joepena@protonmail.com>
2018-09-24 13:27:41 -0600

    added validate password method

--- models/user.js
+++ models/user.js
@@ -16,4 +16,8 @@ userSchema.set('toObject', {
   }
 });
 
+userSchema.methods.validatePassword = function(password) {
+  return password === this.password;
+};
+
 module.exports = mongoose.model('User', userSchema);
