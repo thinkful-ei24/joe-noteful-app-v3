8cadb8fa232ab29845e13fd7a430bf23e3cd4453
Joe-Pena <joepena@protonmail.com>
2018-09-25 12:40:24 -0600

    Add createAuthToken funtion

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET, JWT_EXPIRY} = require('../config');
 
 const User = require('../models/user');
 
@@ -9,9 +11,17 @@ const router = express.Router();
 const options = {session: false, failWithError: true};
 const localAuth = passport.authenticate('local', options);
 
+function createAuthToken (user) {
+  return jwt.sign({user}, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
 //POST============================================================
 router.post('/', localAuth, (req, res, next) => {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
 });
 
 module.exports = router;
